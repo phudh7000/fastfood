a8f31fe388be1be942024be03585449490690915
phudh7000 <78036536+phudh7000@users.noreply.github.com>
2021-08-21 00:37:54 +0700

    fix index

--- src/app/config/db.js
+++ src/app/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 async function connect(){
     try {
         // await mongoose.connect('mongodb://localhost:27017/test', {
-        await mongoose.connect('mongodb+srv://hoangphu:hoangphu@cluster0.fqlac.mongodb.net/test?retryWrites=true&w=majority', {
+        // await mongoose.connect('mongodb+srv://hoangphu:hoangphu@cluster0.fqlac.mongodb.net/test?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://hoangphu:hoangphu@cluster0.fqlac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
